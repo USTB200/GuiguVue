041ef82a73395286ed216ee58cf3f62f8acd0dab
王宇航 <13170815+wang-yuhang0319@user.noreply.gitee.com>
2024-10-15 10:49:57 +0800

    feat:登录路由配置

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from '@/App.vue'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
 import axios from 'axios'
+import router from './router'
 // 配置element-plus国际化
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 // 获取应用实例对象
@@ -17,14 +18,16 @@ import gloablComponent from './components/index';
 app.use(gloablComponent);
 // 测试代码 测试假接口
 // 登录
-axios({
-    url: '/api/user/login',//请求地址
-    method: 'post',//请求方式
-    data: {
-        username: 'admin',
-        password: '111111'
-    }
-})
+// axios({
+//     url: '/api/user/login',//请求地址
+//     method: 'post',//请求方式
+//     data: {
+//         username: 'admin',
+//         password: '111111'
+//     }
+// })
+// 注册模版路由
+app.use(router);
 // 将应用挂载到挂载点上
 app.mount('#app')
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>我是一级路由登录后的首页</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
